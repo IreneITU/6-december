ce308092c09f9d89d85eaddf7fef31c20c7d9d2a
emilead <71696219+emilead@users.noreply.github.com>
2020-12-15 15:55:04 +0100

    Add files via upload

---
+++ components/utils/ListOfMarkers.js
@@ -0,0 +1,91 @@
+export const markers = [{
+  coordinates: {
+    latitude: 55.641267,
+    longitude: 12.517784
+  },
+},
+{
+    coordinates: {
+      latitude: 55.671623,
+      longitude: 12.605781
+    },
+  },
+  {
+    coordinates: {
+      latitude: 55.664843,
+      longitude: 12.573970
+    },
+  },
+  {
+    coordinates: {
+      latitude: 55.661844,
+      longitude: 12.577625
+    },
+  },
+  {
+    coordinates: {
+      latitude: 55.659118,
+      longitude: 12.581054
+    },
+  },
+  {
+    coordinates: {
+      latitude: 55.656963,
+      longitude: 55.656963
+    },
+  },
+  {
+    coordinates: {
+      latitude: 55.654261,
+      longitude: 12.581082
+    },
+  },
+  { 
+    coordinates: {
+      latitude: 55.664508,
+      longitude: 12.580496
+    },
+  },
+  { 
+    coordinates: {
+      latitude: 55.665555,
+      longitude: 12.575142
+    },
+  },
+  { 
+    coordinates: {
+      latitude: 55.674448,
+      longitude: 12.558636
+    },
+  },
+  { 
+    coordinates: {
+      latitude: 55.677885,
+      longitude: 12.556499
+    },
+  },
+  { 
+    coordinates: {
+      latitude: 55.680849,
+      longitude: 12.560136
+    },
+  },
+  { 
+    coordinates: {
+      latitude: 55.687037,
+      longitude: 12.562818
+    },
+  },
+  { 
+    coordinates: {
+      latitude: 55.692445,
+      longitude: 12.570091
+    },
+  },
+{
+  coordinates: {
+    latitude: 55.681547,
+    longitude: 12.575751201
+  }
+}
+];
